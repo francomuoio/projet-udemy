55a786eebb119dc624ddd71b2405259ea16093cb
francomuoio <franco.59171@gmail.com>
2020-02-23 20:23:48 +0100

    d

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'statique/team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'statique#acceuil'
+  root 'compteur#index'
 end
